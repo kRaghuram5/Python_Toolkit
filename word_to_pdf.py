7481e85ac0b3891100fd9a92006744656e1cca0f
K RAGHURAM <raghuram1182005@gmail.com>
2025-03-07 21:08:40 +0530

    Updated

--- word_to_pdf.py
+++ word_to_pdf.py
@@ -1,8 +1,10 @@
+# This to convert from WORD to PDF
+# --REQUIRMENTS ** Document file(.docx) to be converted
 import comtypes.client
 
 word = comtypes.client.CreateObject("Word.Application")
-doc = word.Documents.Open("E:\\Doc1.docx")
-doc.SaveAs("output2.pdf", FileFormat=17)  # 17 = PDF format
+doc = word.Documents.Open("example.docx") #-- Enter the document name here!!
+doc.SaveAs("output.pdf", FileFormat=17)  # Enter the output file name here!!
 doc.Close()
 word.Quit()
 
