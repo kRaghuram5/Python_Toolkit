1ee2595851a2aca2d57c0b72abe20aa7a7e1fc47
K RAGHURAM <raghuram1182005@gmail.com>
2025-03-07 20:47:51 +0530

    Updated

--- PDF_to_Text.py
+++ PDF_to_Text.py
@@ -1,14 +1,14 @@
-'''1. Extract Text or Images
-Library: PyPDF2, PyMuPDF (also called Fitz), or pdfminer.six
-For image extraction: Use PyMuPDF or pdf2image.
-Example:'''
+'''This is to Extract Text or Images
+using Library: PyPDF2, PyMuPDF (also called Fitz), or pdfminer.six
+For image extraction: Use PyMuPDF or pdf2image.'''
+#--REQUIRMENTS **The pdf file that to be extract 
 import PyPDF2
 
 # Open PDF file
-pdf_file = open(r'E:\Resume.pdf', 'rb')
+pdf_file = open(r'example.pdf', 'rb')# --Enter the file name or path
 pdf_reader = PyPDF2.PdfReader(pdf_file)
 #writing in text file
-output=open('output.txt','w')
+output=open('output.txt','w')# Enter the file name to be create to text
 
 # Extract text from each page
 for page in pdf_reader.pages:
