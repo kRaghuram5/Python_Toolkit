91770e46b79b68c2255e38f53d00c6cc564ed0ab
K RAGHURAM <raghuram1182005@gmail.com>
2025-03-07 20:43:46 +0530

    Updated

--- PDF_TO_WORD.py
+++ PDF_TO_WORD.py
@@ -1,10 +1,13 @@
+# This is to convert PDF file to Word format 
+# --REQUIRMENTS **The PDF file that to be converted to word document.
+
 from pdf2docx import Converter
 
-pdf_file = "example.1.pdf"
-docx_file = "output.docx"
+pdf_file = "example.1.pdf" #--Enter the file name or path here!!
+docx_file = "output.docx"  #--Enter the document file name to be created
 
 converter = Converter(pdf_file)
-converter.convert(docx_file, start=0, end=None)  # Converts the entire PDF
+converter.convert(docx_file, start=0, end=None) # converts the entire document, You can also change the constraints!!
 converter.close()
 
 print("PDF converted to Word successfully!")
