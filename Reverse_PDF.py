0bb68c38101b4b4dbbc579127866f655968d6dac
K RAGHURAM <raghuram1182005@gmail.com>
2025-03-07 21:01:06 +0530

    Updated

--- Reverse_PDF.py
+++ Reverse_PDF.py
@@ -1,13 +1,13 @@
-'''Reverse Page Numbers
-Library: PyPDF2'''
+# --This is to Reverse Page Numbers
+# REQUIRMENTS **The pdf file that need to be reversed**
 from PyPDF2 import PdfReader, PdfWriter
 
-reader = PdfReader("example.1.pdf")
+reader = PdfReader("example.1.pdf") #--Enter the name of pdf name or path here
 writer = PdfWriter()
 
 # Reverse order
 for page in reversed(reader.pages):
     writer.add_page(page)
 
-with open("reordered.pdf", "wb") as f:
+with open("reversed.pdf", "wb") as f: #--Enter the name of file name or path
     writer.write(f)
