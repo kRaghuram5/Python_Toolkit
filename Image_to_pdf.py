88feafcd368208294bf547a03c87cb9646063f1f
K RAGHURAM <raghuram1182005@gmail.com>
2025-03-07 20:37:10 +0530

    Updated

--- Image_to_pdf.py
+++ Image_to_pdf.py
@@ -1,6 +1,8 @@
+#This is for Converting image To PDF
+# --REQUIRMNETS **Enter the One or More Image with Name or Respective path in the form of list**
 from PIL import Image
 
-images = ["page_1.png", "page_2.png"]  # List of image file paths
+images = ["page_1.png", "page_2.png"]  # Enter the List of image name or file paths here
 pdf_path = "output.pdf"
 
 image_objects = [Image.open(img).convert("RGB") for img in images]
